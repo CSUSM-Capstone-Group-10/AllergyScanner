c284b54f55b88fd2fc30cdd54ccf19eb670eeff5
Josue-B-Navarrete <josuedrew88@gmail.com>
2025-03-22 18:20:21 -0700

    Split main into two fragments (Allergy Selection | Information). Main now just handles the view between the two. Added two additional .kt files for them and xml files Updated bryces changes to Allergy selection with more allergies Added general Information with the updated food allergy categories (in the same order) Added a card in the general section to include lucas disclaimer at the bottom

--- app/src/main/java/com/example/allergyscanner/MainActivity.kt
+++ app/src/main/java/com/example/allergyscanner/MainActivity.kt
@@ -1,245 +1,48 @@
 package com.example.allergyscanner
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.example.allergytest.R
 import com.example.allergytest.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var adapter: AllergenAdapter
-    private var allergens = listOf(
-        AllergenCategory(
-            "Dairy",
-            mutableListOf(
-                AllergenItem("Butter"),
-                AllergenItem("Butter Fat"),
-                AllergenItem("Butter Oil"),
-                AllergenItem("Buttermilk"),
-                AllergenItem("Artificial Butter Flavor"),
-                AllergenItem("Casein"),
-                AllergenItem("Caseinates"),
-                AllergenItem("Cheese"),
-                AllergenItem("Cream"),
-                AllergenItem("Cottage Cheese"),
-                AllergenItem("Curds"),
-                AllergenItem("Custard"),
-                AllergenItem("Ghee"),
-                AllergenItem("Half & Half"),
-                AllergenItem("Hydrolysates"),
-                AllergenItem("Lactalbumin"),
-                AllergenItem("Lactalbumin Phosphate"),
-                AllergenItem("Lactoglobulin"),
-                AllergenItem("Lactose"),
-                AllergenItem("Lactulose"),
-                AllergenItem("Milk"),
-                AllergenItem("Nougat"),
-                AllergenItem("Pudding"),
-                AllergenItem("Rennet Casein"),
-                AllergenItem("Sour Cream"),
-                AllergenItem("Sour Cream Solids"),
-                AllergenItem("Whey"),
-                AllergenItem("Yogurt")
-            )
-        ),
-        AllergenCategory(
-            "Eggs",
-            mutableListOf(
-                AllergenItem("Albumin"),
-                AllergenItem("Egg"),
-                AllergenItem("Egg Substitutes"),
-                AllergenItem("Eggnog"),
-                AllergenItem("Globulin"),
-                AllergenItem("Livetin"),
-                AllergenItem("Lysozyme"),
-                AllergenItem("Mayonnaise"),
-                AllergenItem("Meringue"),
-                AllergenItem("Ovalbumin"),
-                AllergenItem("Ovomucoid"),
-                AllergenItem("Ovovitellin"),
-                AllergenItem("Simplesse"),
-                AllergenItem("Surimi"),
-                AllergenItem("Lecithin"),
-                AllergenItem("Marzipan"),
-                AllergenItem("Marshmallows"),
-                AllergenItem("Pasta"),
-                AllergenItem("Egg Flavors")
-            )
-        ),
-        AllergenCategory(
-            "Peanuts",
-            mutableListOf(
-                AllergenItem("Beer Nuts"),
-                AllergenItem("Peanut Oil"),
-                AllergenItem("Ground Nuts"),
-                AllergenItem("Mixed Nuts"),
-                AllergenItem("Monkey Nuts"),
-                AllergenItem("Nu-Nuts Flavored Nuts"),
-                AllergenItem("Nut Pieces"),
-                AllergenItem("Peanut"),
-                AllergenItem("Peanut Butter"),
-                AllergenItem("Peanut Flour"),
-                AllergenItem("Peanut Protein"),
-                AllergenItem("Hydrolyzed Peanut Protein"),
-                AllergenItem("Egg Rolls"),
-                AllergenItem("Marzipan"),
-                AllergenItem("Nougat"),
-                AllergenItem("Sunflower Seeds"),
-            )
-        ),
-        AllergenCategory(
-            "Soybeans",
-            mutableListOf(
-                AllergenItem("Hydrolyzed Soy Protein"),
-                AllergenItem("Miso"),
-                AllergenItem("Shoyu Sauce"),
-                AllergenItem("Soy"),
-                AllergenItem("Soya"),
-                AllergenItem("Soybean"),
-                AllergenItem("Soy Protein"),
-                AllergenItem("Soy Sauce"),
-                AllergenItem("Tamari"),
-                AllergenItem("Tempeh"),
-                AllergenItem("Textured Vegetable Protein (TVP)"),
-                AllergenItem("Tofu")
-            )
-        ),
-        AllergenCategory(
-            "Tree Nuts",
-            mutableListOf(
-                AllergenItem("Almonds"),
-                AllergenItem("Brazil Nuts"),
-                AllergenItem("Caponata"),
-                AllergenItem("Cashews"),
-                AllergenItem("Chestnuts"),
-                AllergenItem("Hazelnut"),
-                AllergenItem("Gianduja"),
-                AllergenItem("Hickory Nuts"),
-                AllergenItem("Macadamia Nuts"),
-                AllergenItem("Marzipan"),
-                AllergenItem("Nougat"),
-                AllergenItem("Nu-Nut"),
-                AllergenItem("Nut Meal"),
-                AllergenItem("Nut Oil"),
-                AllergenItem("Nut Paste"),
-                AllergenItem("Nut Pieces"),
-                AllergenItem("Pecans"),
-                AllergenItem("Pesto"),
-                AllergenItem("Pine Nuts"),
-                AllergenItem("Pistachios"),
-                AllergenItem("Walnuts")
-            )
-        ),
-        AllergenCategory(
-            "Wheat",
-            mutableListOf(
-                AllergenItem("Bran"),
-                AllergenItem("Bread Crumbs"),
-                AllergenItem("Bulgar"),
-                AllergenItem("Cereal Extract"),
-                AllergenItem("Couscous"),
-                AllergenItem("Cracker Meal"),
-                AllergenItem("Durum"),
-                AllergenItem("Durum Flour"),
-                AllergenItem("Enriched Flour"),
-                AllergenItem("Farina"),
-                AllergenItem("Flour"),
-                AllergenItem("Gluten"),
-                AllergenItem("Kamut"),
-                AllergenItem("Seitan"),
-                AllergenItem("Semolina"),
-                AllergenItem("Spelt"),
-                AllergenItem("Vital Gluten"),
-                AllergenItem("Wheat"),
-                AllergenItem("Whole Wheat Berries"),
-                AllergenItem("Whole Wheat Flour")
-            )
-        ),
-        AllergenCategory(
-            "Crustaceans and Shellfish",
-            mutableListOf(
-                AllergenItem("Crab"),
-                AllergenItem("Crawfish"),
-                AllergenItem("Lobster"),
-                AllergenItem("Prawns"),
-                AllergenItem("Shrimp"),
-                AllergenItem("Snails"),
-                AllergenItem("Abalone"),
-                AllergenItem("Clams"),
-                AllergenItem("Mussels"),
-                AllergenItem("Oysters"),
-                AllergenItem("Scallops")
-            )
-        ),
-        AllergenCategory(
-            "Fish",
-            mutableListOf(
-                AllergenItem("Bass"),
-                AllergenItem("Catfish"),
-                AllergenItem("Cod"),
-                AllergenItem("Halibut"),
-                AllergenItem("Herring"),
-                AllergenItem("Mackerel"),
-                AllergenItem("Octopus"),
-                AllergenItem("Pollock"),
-                AllergenItem("Sardines"),
-                AllergenItem("Salmon"),
-                AllergenItem("Snapper"),
-                AllergenItem("Squid"),
-                AllergenItem("Swordfish"),
-                AllergenItem("Tilapia"),
-                AllergenItem("Trout"),
-                AllergenItem("Tuna")
-            )
-        ),
-    )
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //Load previously saved selections
-        loadSelectedAllergens()
+        // Set up ViewPager with Fragments
+        viewPager = binding.viewPager
+        viewPager.adapter = AllergySectionsPagerAdapter(this)
 
-        //Keep existing allergen selection functionality
-        adapter = AllergenAdapter(this, allergens.toMutableList(), binding.allergenExpandableList)
-        binding.allergenExpandableList.setAdapter(adapter)
-
-        //Set up Save Button functionality
-        val saveButton: Button = findViewById(R.id.saveButton)
-        saveButton.setOnClickListener {
-            saveSelectedAllergens()
-        }
-
-
-        //search bar functionality
-        val searchBar: EditText = findViewById(R.id.search_bar)
-        searchBar.addTextChangedListener(object: TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                filterAllergens(s.toString()) //calls filter allergens function when text is changed in search bar
+        // Set up TabLayout with ViewPager
+        tabLayout = binding.tabLayout
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "Allergy Selection"
+                1 -> "Information"
+                else -> null
             }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-        })
+        }.attach()
+
         // Bottom Navigation Setup
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNav)
+        val bottomNavigationView: BottomNavigationView = binding.bottomNav
         bottomNavigationView.selectedItemId = R.id.navAllergens
 
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navAllergens -> {
-                    // Handle "Allergens" selection
-                    startActivity(Intent(this, MainActivity::class.java))
+                    // Already on Allergens screen
                     true
                 }
                 R.id.navCamera -> {
@@ -256,49 +59,4 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-    //Save Selected Allergens to SharedPreferences
-    private fun saveSelectedAllergens() {
-        val sharedPreferences = getSharedPreferences("AllergenPrefs", Context.MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-
-        val selectedAllergens = allergens
-            .flatMap { it.items }
-            .filter { it.isSelected }
-            .map { it.name }
-
-        editor.putStringSet("selectedAllergens", selectedAllergens.toSet())
-        editor.apply()
-
-        // Show a confirmation message
-        Toast.makeText(this, "Allergen selection saved!", Toast.LENGTH_SHORT).show()
-    }
-
-    //Load Previously Saved Allergens
-    private fun loadSelectedAllergens() {
-        val sharedPreferences = getSharedPreferences("AllergenPrefs", Context.MODE_PRIVATE)
-        val savedAllergens = sharedPreferences.getStringSet("selectedAllergens", emptySet()) ?: emptySet()
-
-        allergens.forEach { category ->
-            category.items.forEach { subItem ->
-                subItem.isSelected = savedAllergens.contains(subItem.name)
-            }
-            category.isSelected = category.items.all { it.isSelected } //If all subitems are selected, check category
-        }
-    }
-
-    private fun filterAllergens(query: String) {
-        val filteredAllergens = allergens.map { category ->
-            val filteredItems = category.items.filter { it.name.contains(query, ignoreCase = true) }
-                .toMutableList()
-            AllergenCategory(category.name, filteredItems)
-        }.filter { it.items.isNotEmpty() }
-
-        adapter.updateData(filteredAllergens)
-        //expands view of allergen list when searching, so user can see results
-        for (i in 0 until adapter.groupCount) {
-            binding.allergenExpandableList.expandGroup(i)
-        }
-    }
 }
-
